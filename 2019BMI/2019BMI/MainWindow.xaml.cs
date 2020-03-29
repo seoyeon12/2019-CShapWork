dddc7dd772953a01e72c6d12b8e792022dd26cb9
seoyeon <35373729+seoyeon12@users.noreply.github.com>
2020-03-29 15:30:04 +0900

    Add files via upload

---
+++ 2019BMI/2019BMI/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2019BMI
+{
+    public partial class MainWindow : Window
+    {
+        Double weight = 0.0f;
+        Double height = 0.0f;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BMILineClear();
+            height = double.Parse(InputHeight.Text);
+            weight = double.Parse(InputWeight.Text);
+
+            Double result = Calculate(height / 100f,weight);
+            CheckingBMI(result);
+        }
+        private void Button_Clear_Click(object sender, RoutedEventArgs e)
+        {
+            InputHeight.Text = "";
+            InputWeight.Text = "";
+            resultBMI.Text = "";
+            BMILineClear();
+        }
+        private Double Calculate(Double height, Double weight)
+        {
+            return Math.Round(weight / (height * height),2);
+        }
+        private void CheckingBMI(Double result)
+        {
+            if(result <= 18.5)
+            {
+                resultBMI.Text = result + "( 저체중 )";
+                resultBMI.Foreground = Brushes.DarkSalmon;
+                minerWeight.Visibility = Visibility;
+            }
+            else if(result > 18.5 && result <= 23)
+            {
+                resultBMI.Text = result + "( 정상 )";
+                resultBMI.Foreground = Brushes.Green;
+                okWeight.Visibility = Visibility;
+            }
+            else if(result > 23 && result <= 25)
+            {
+                resultBMI.Text = result + "( 과체중 )";
+                resultBMI.Foreground = Brushes.Orange;
+                overWeight.Visibility = Visibility;
+            }
+            else if (result > 25 && result <= 30)
+            {
+                resultBMI.Text = result + "( 비만 )";
+                resultBMI.Foreground = Brushes.Red;
+                majerWeight.Visibility = Visibility;
+            }
+            else if(result > 30 && result <= 35)
+            {
+                resultBMI.Text = result + "( 고도비만 )";
+                resultBMI.Foreground = Brushes.Red;
+                upMajerWeight.Visibility = Visibility;
+            }
+            else if(result > 35 && result <= 40)
+            {
+                resultBMI.Text = result + "( 초고도비만 )";
+                resultBMI.Foreground = Brushes.Red;
+                upupMajerWeight.Visibility = Visibility;
+            }
+        }
+        private void BMILineClear()
+        {
+            minerWeight.Visibility = Visibility.Hidden;
+            okWeight.Visibility = Visibility.Hidden;
+            overWeight.Visibility = Visibility.Hidden;
+            majerWeight.Visibility = Visibility.Hidden;
+            upMajerWeight.Visibility = Visibility.Hidden;
+            upupMajerWeight.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ 2019SkidMark/SkideMark/SkideMark/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SkideMark
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Double skidMark;
+        Double friction;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            skidMark = double.Parse(InputSkidMark.Text);
+            friction = double.Parse(InputFriction.Text);
+
+            Double result = Math.Round(Calculate(skidMark, friction), 2);
+
+            TextBlock recodeBlock = new TextBlock();
+            recodeBlock.Text = result.ToString();
+
+            Recode.Children.Add(recodeBlock);
+            NowCalculationResult.Text = result.ToString() + " km/h";
+
+            //if (intCheck(skidMark) && intCheck(friction))
+            //{
+            //    Double result = Math.Round(Calculate(skidMark, friction), 2);
+
+            //    TextBlock recodeBlock = new TextBlock();
+            //    recodeBlock.Text = result.ToString();
+
+            //    Recode.Children.Add(recodeBlock);
+            //    NowCalculationResult.Text = result.ToString() + " km/h";
+            //    return;
+            //}
+            //else
+            //{
+            //    MessageBox.Show(" 다시 입력해주십시오. ");
+            //    InputSkidMark.Clear();
+            //    InputFriction.Clear();
+            //}
+        }
+
+        private Double Calculate(Double skidMark, Double friction)
+        {
+            return Math.Sqrt(254 * skidMark * friction);
+        }
+
+        //private Boolean intCheck(Double cont)
+        //{
+        //    string strNum;
+        //    strNum = Regex.Replace(cont.ToString(), @"\d", "");
+        //    //입력받은 값 중에 문자만 받아오기
+        //    if (strNum != "") //문자가 있다면 false을 반환
+        //    {
+        //        return false;
+        //    }
+        //    return true;
+        //}
+
+        private void InputCheck(object sender, KeyEventArgs e)
+        {
+            TextBox txt = sender as TextBox;
+
+            if (e.Key == Key.OemPeriod)
+            {
+                if (txt.Text.Contains("."))
+                {
+                    MessageBox.Show("소수점이 이미 존재합니다.");
+                    e.Handled = true;
+                }
+                return;
+            }
+
+            if (!(((Key.D0 <= e.Key) && (e.Key <= Key.D9)) || e.Key == Key.Back))
+            {
+                MessageBox.Show("숫자만 입력해 주십시오");
+                e.Handled = true;
+            }
+        }
+    }
+}
